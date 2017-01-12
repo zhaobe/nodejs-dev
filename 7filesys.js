166bb1341d4053492c5cab923fa5d4c286e758cb
zhaobe <zhaoben777@gmail.com>
2017-01-11 21:56:50 -0800

    Added parameter summary of more file system methods as comments.

--- 7filesys.js
+++ 7filesys.js
@@ -1,19 +1,22 @@
 var fs = require("fs");
 
+/***
 //asynchronous read
-//fs.readFile('input.txt', function (err, data) {
-//   if (err) {
-//      return console.error(err);
-//   }
-//   console.log("Asynchronous read: " + data.toString());
-//});
+fs.readFile('input.txt', function (err, data) {
+   if (err) {
+      return console.error(err);
+   }
+   console.log("Asynchronous read: " + data.toString());
+});
 
 //synchronous read
-//var data = fs.readFileSync('input.txt');
-//console.log("Synchronous read: " + data.toString());
-//
-//console.log("End of program");
+var data = fs.readFileSync('input.txt');
+console.log("Synchronous read: " + data.toString());
 
+console.log("End of program");
+***/
+
+//writing to file example
 console.log("first console log.");
 fs.writeFile('input.txt', 'text to be the input', function(err) {
     if(err) {
@@ -31,3 +34,14 @@ fs.writeFile('input.txt', 'text to be the input', function(err) {
         console.log("Async read: " + data.toString());
     });
 });
+
+//other  file sys methods:
+//get file info - methods return boolean
+//reading a file - fs.read(fd, buffer, offset, length, position, callback)
+//closing a file - fs.close(fd, callback)
+//truncate a file - fs.truncate(fd, length, callback)
+//delete a file - fs.unlink(path, callback)
+
+//create a directory - fs.mkdir(path[, mode], callback)
+//read a directory - fs.readdir(path, callback)
+//remove a directory - fs.rmdir(path, callback)
