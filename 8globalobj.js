0b0288a10f2aba3dbc69881796c89b648540032d
zhaobe <zhaoben777@gmail.com>
2017-01-12 23:55:44 -0800

    Adding global objects example.

---
+++ 8globalobj.js
@@ -0,0 +1,13 @@
+//console.log(__filename);
+//console.log(__dirname);
+
+function printFunc() {
+   console.log( "Hello");
+}
+
+//setTimeout(printFunc, 2000);
+
+//var myVar = setTimeout(printFunc, 2000);
+//clearTimeout(myVar);
+
+//setInterval(printFunc, 1000);
